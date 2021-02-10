2ed8b4b3f6b0b32b15f64c2f93e9b0d9479134c0
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-02-10 22:07:00 +0100

    Product Table seed

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ProductsTableSeeder::class);
+        \App\Models\Product::factory()->count(10)->create();
     }
 }
